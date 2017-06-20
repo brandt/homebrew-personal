13164e0d506a1add8507e09847c8a8dc4c8c98a0
J. Brandt Buckley <brandt@runlevel1.com>
2017-06-20 06:06:20 -0600

    Add formula for gmime 2.6 version

---
+++ gmime@2.6.rb
@@ -0,0 +1,56 @@
+# Based on the last gmime 2.6 revision in homebrew/core
+#
+# License: BSD 2-Clause
+#
+# Copyright (c) 2009-present, Homebrew contributors
+# All rights reserved.
+class GmimeAT26 < Formula
+  desc "MIME mail utilities"
+  homepage "https://spruce.sourceforge.io/gmime/"
+  url "https://download.gnome.org/sources/gmime/2.6/gmime-2.6.23.tar.xz"
+  sha256 "7149686a71ca42a1390869b6074815106b061aaeaaa8f2ef8c12c191d9a79f6a"
+
+  depends_on "pkg-config" => :build
+  depends_on "libgpg-error" => :build
+  depends_on "gobject-introspection" => :recommended
+  depends_on "glib"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-largefile
+      --disable-vala
+      --disable-mono
+      --disable-glibtest
+    ]
+
+    if build.with? "gobject-introspection"
+      args << "--enable-introspection"
+    else
+      args << "--disable-introspection"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <gmime/gmime.h>
+      int main (int argc, char **argv)
+      {
+        g_mime_init(0);
+        if (gmime_major_version>=2) {
+          return 0;
+        } else {
+          return 1;
+        }
+      }
+      EOS
+    flags = `pkg-config --cflags --libs gmime-2.6`.split
+    system ENV.cc, "-o", "test", "test.c", *(flags + ENV.cflags.to_s.split)
+    system "./test"
+  end
+end
