9e74c3ab24042d7d52b94800571a7625d35cdcad
J. Brandt Buckley <brandt@runlevel1.com>
2019-09-17 07:41:13 -0600

    Add launchd plist and caveats for awsresolver

--- awsresolver.rb
+++ awsresolver.rb
@@ -36,6 +36,44 @@ class Awsresolver < Formula
     end
   end
 
+  def caveats
+    <<~EOS
+      If you want DNS queries for "*.internal" to be automatically sent to
+      awsresolver, run this command:
+
+          sudo awsresolver setup
+    EOS
+  end
+
+  # While this could run at startup, at the moment it seems preferable to run
+  # it as the regular, unprivileged user when they login.
+  plist_options :startup => false, :manual => "awsresolver run"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/awsresolver</string>
+          <string>run</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/awsresolver.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/awsresolver.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
   test do
     system bin/"awsresolver", "version"
   end
